e0319b1abd123b41ba99fd2889d01d4d79ccbc47
tao123456789 <2413629661@qq.com>
2021-03-25 15:21:11 +0800

    2021-03-25 引入element-ui手脚架

--- src/components/pzsp_detail.vue
+++ src/components/pzsp_detail.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="zrpd">
     <label for="gys_zr">
-      <input type="radio" id="gys_zr" value="供应商责任" v-model="zr">供应商责任
+      <input type="radio" id="gys_zr" value="供应商责任" >供应商责任
+<!--      v-model="zr"-->
     </label>
     <label>
-      <input type="radio" id="sany_zr" value="三一责任" v-model="zr">三一责任
+      <input type="radio" id="sany_zr" value="三一责任" >三一责任
+<!--      v-model="zr"-->
     </label>
   </div>
 </template>
